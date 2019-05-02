a92e54e636f08c190ae12e82720d9e0e9795c6c6
Alexander Zemskov <zaismit@gmail.com>
2019-05-02 17:58:50 +0300

    Create 5_show_our_peoples.php

---
+++ 5_show_our_peoples.php
@@ -0,0 +1,36 @@
+<?php
+class friendship
+{
+    public $name;
+    public $age;
+    public $mobile;
+    public $hobby;
+    
+    function show_friendship()
+    {
+        
+    }
+    function add_friendship()
+    {
+        
+    }
+    function update_friendship() 
+    {
+        
+    }
+    function delete_friendship()
+    {
+        
+    }
+}
+
+$friendship_one = new friendship;
+
+$firstfriend = ($friendship_one -> name = "Alex");
+
+echo $firstfriend;
+
+$secondfriend = ($friendship_one -> name = "Olga");
+
+echo $secondfriend;
+?>
