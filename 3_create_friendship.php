01ccf2aef4e538bc9a0c76e68532b66877e458eb
Alexander Zemskov <zaismit@gmail.com>
2019-05-02 17:40:01 +0300

    Create 3_create_friendship.php

---
+++ 3_create_friendship.php
@@ -0,0 +1,30 @@
+<?php
+class friendship
+{
+      
+  function show_friendship()
+	{
+
+	}
+
+	function add_friendship()
+	{
+
+	}
+
+	function update_friendship() 
+	{
+
+	}
+
+	function delete_friendship()
+	{
+
+	}
+}
+
+$friendship_one = new friendship;
+
+var_dump($friendship_one);
+
+?>
